1da89b935600a6db7f034bb753661079235c72f4
desmet <lexdesmet@gmail.com>
2014-03-14 14:37:55 +0400

    Updating project structure.

---
+++ QE_Interfaces_ex1/src/Interfaces/Ex1/CSimpl.java
@@ -0,0 +1,22 @@
+package Interfaces.Ex1;
+
+public class CSimpl implements CharSequence {
+
+
+
+    public int length() {
+        return 0;
+    }
+
+    public char charAt(int index) {
+        return 0;
+    }
+
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    public static void main(String... args) {
+
+    }
+}
