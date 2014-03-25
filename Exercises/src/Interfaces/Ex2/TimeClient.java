a6004926f133675fe11bf67d54e5471eba82ed43
desmet <lexdesmet@gmail.com>
2014-03-25 18:25:46 +0400

    ~

--- Exercises/src/Interfaces/Ex2/TimeClient.java
+++ Exercises/src/Interfaces/Ex2/TimeClient.java
@@ -1,6 +1,6 @@
 package Interfaces.Ex2;
 
-//import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
 /**
  * Suppose you have written a time server that periodically notifies its clients of the
@@ -12,5 +12,5 @@ public interface TimeClient {
     void setDate(int day, int month, int year);
     void setTime(int hour, int minute, int second);
     void setDateAndTime(int day, int month, int year, int hour, int minute, int second);
-//    DateTime getLocalDateTime();
+    LocalDateTime getLocalDateTime();
 }
